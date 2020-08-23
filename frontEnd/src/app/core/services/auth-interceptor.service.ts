6cd381f5f942868c9d97b921940a38afc41ffe5d
pablotolentino@outlook.com <PabloTolentino@outlook.com>
2020-08-23 14:57:28 -0500

    se modico la clase de HttpInterceptor

--- frontEnd/src/app/core/services/auth-interceptor.service.ts
+++ frontEnd/src/app/core/services/auth-interceptor.service.ts
@@ -11,23 +11,24 @@ export class AuthInterceptorService implements HttpInterceptor {
   constructor() { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token: string = localStorage.getItem('token');
     let request = req;
-
-    if (token) {
-      request = req.clone({
-        setHeaders: {
-          authorization: `Bearer ${ token }`
-        }
-      });
-    }
-
-    const nextReq = req.clone(
-    //   {
-    //   headers: req.headers.set('X-API-KEY', environment.API_KEY)
+    
+    // const token: string = localStorage.getItem('token');
+    // if (token) {
+    //   request = req.clone({
+    //     setHeaders: {
+    //       authorization: `Bearer ${ token }`
+    //     }
+    //   });
     // }
-    );
-    return next.handle(nextReq).pipe(map((event: HttpEvent<any>) => {
+
+    // const nextReq = request.clone(
+    //   {
+    //     headers: request.headers.set('X-API-KEY', environment.API_KEY)
+    //   }
+    // );
+
+    return next.handle(request).pipe(map((event: HttpEvent<any>) => {
       return event;
     }),
       catchError((err: HttpErrorResponse) => {
